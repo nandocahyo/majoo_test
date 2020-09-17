cd1c0eb0566bc8ffbccadcaa2669cadbdff67b63
nandocahyo <nandocahyo77@gmail.com>
2020-09-17 16:49:39 +0700

    dashboard

---
+++ app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('pages.admin.dashboard');
+    }
+
+}
---
+++ resources/views/pages/admin/dashboard.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.admin')
+
+@section('content')
+ <!-- Begin Page Content -->
+ <div class="container-fluid">
+
+<!-- Page Heading -->
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+  <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+</div>
+
+<!-- Content Row -->
+<div class="row">
+<!-- grooming card section -->
+  <!-- Paket Grooming Card -->
+  <div class="col-xl-6 col-md-3 mb-4">
+    <div class="card border-left-primary shadow h-100 py-2">
+      <div class="card-body">
+        <div class="row no-gutters align-items-center">
+          <div class="col mr-2">
+            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Produk</div>
+            <div class="h5 mb-0 font-weight-bold text-gray-800">5</div>
+          </div>
+          <div class="col-auto">
+            <i class="fas fa-box fa-2x text-gray-300"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Order Card -->
+  <div class="col-xl-6 col-md-3 mb-4">
+    <div class="card border-left-success shadow h-100 py-2">
+      <div class="card-body">
+        <div class="row no-gutters align-items-center">
+          <div class="col mr-2">
+            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Order</div>
+            <div class="h5 mb-0 font-weight-bold text-gray-800">5</div>
+          </div>
+          <div class="col-auto">
+            <i class="fas fa-shopping-cart fa-2x text-gray-300"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- end grooming card section -->          
+</div>
+
+
+      
+
+     
+    
+<!-- Content Row -->
+</div>
+<!-- /.container-fluid -->
+@endsection
