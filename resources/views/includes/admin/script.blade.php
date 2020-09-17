2d9308fb216db4ee866aeb40604e7a566a878c96
nandocahyo <nandocahyo77@gmail.com>
2020-09-17 14:51:11 +0700

    admin blade

---
+++ resources/views/includes/admin/script.blade.php
@@ -0,0 +1,56 @@
+ <!-- Bootstrap core JavaScript-->
+ <script src="{{ url('backend/vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ url('backend/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="{{ url('backend/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="{{ url('backend/js/sb-admin-2.min.js') }}"></script>
+
+  <!-- Page level plugins -->
+  <script src="{{ url('backend/vendor/chart.js/Chart.min.js') }}"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="{{ url('backend/js/demo/chart-area-demo.js') }}"></script>
+  <script src="{{ url('backend/js/demo/chart-pie-demo.js') }}"></script>
+
+  <!-- Page level plugins -->
+  <script src="{{ url('backend/vendor/datatables/jquery.dataTables.min.js') }}"></script>
+  <script src="{{ url('backend/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="{{ url('backend/js/demo/datatables-demo.js') }}"></script>
+  <script src="{{ url('summernote/summernote-bs4.min.js') }}"></script>
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  <script>
+        function confirmDelete(item_id) {
+            swal({
+                title: "Apa anda yakin ingin menghapus ?",
+                text: "Data yang terhapus tidak bisa dikembalikan lagi !",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            })
+                .then((willDelete) => {
+                    if (willDelete) {
+                        $('#'+item_id.closest('#delete-pack').submit());
+                    } else {
+                        swal("Berhasil dibatalkan");
+                    }
+                });
+        }
+    </script> 
+  <script src="{{ url('dropify/js/dropify.js') }}"></script>
+  <script type="text/javascript">
+        $(document).ready(function(){
+            $('.dropify').dropify();
+        });
+    </script>
+
+
+
+
+
+
+  
---
+++ resources/views/includes/admin/sidebar.blade.php
@@ -0,0 +1,43 @@
+   <!-- Sidebar -->
+   <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+<!-- Sidebar - Brand -->
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="">
+  <div class="sidebar-brand-text mx-3">Majoo Store</div>
+</a>
+
+<!-- Divider -->
+<hr class="sidebar-divider">
+
+<!-- Heading -->
+<div class="sidebar-heading">
+  Order Manajemen
+</div>
+
+<!-- Nav Item - Grooming Order -->
+<li class="nav-item">
+  <a class="nav-link" href="">
+    <i class="fas fa-fw fa-bath"></i>
+    <span>Order Management</span></a>
+</li>
+
+
+<!-- Nav Item - Pet Boarding Order -->
+<li class="nav-item">
+  <a class="nav-link" href="">
+    <i class="fas fa-fw fa-building"></i>
+    <span>Product Management</span></a>
+</li>
+
+
+
+<!-- Divider -->
+<hr class="sidebar-divider d-none d-md-block">
+
+<!-- Sidebar Toggler (Sidebar) -->
+<div class="text-center d-none d-md-inline">
+  <button class="rounded-circle border-0" id="sidebarToggle"></button>
+</div>
+
+</ul>
+<!-- End of Sidebar -->
