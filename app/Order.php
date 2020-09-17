7e6b713d577a2b61457bb91a0c8c099599ca4587
nandocahyo <nandocahyo77@gmail.com>
2020-09-17 17:10:42 +0700

    product and order  model

---
+++ app/Order.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'id_product','name_customer','address','email','phone_number','qty','total_price'
+    ];
+
+    protected $hidden = [
+
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'id_product','id');
+    }
+
+}
